fa5256ff2db298a694fdfe66a6c4a2a31532f92e
Maria Belikova <mebelikova@edu.hse.ru>
2024-06-23 21:30:53 +0300

    Minor codestyle fixes

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	h "github.com/belikoooova/url-shortener/internal/app/handler"
-	shortener2 "github.com/belikoooova/url-shortener/internal/app/shortener"
+	short "github.com/belikoooova/url-shortener/internal/app/shortener"
 	stor "github.com/belikoooova/url-shortener/internal/app/storage"
 	"net/http"
 )
 
 func main() {
-	var shortener shortener2.Shortener = shortener2.NewSha256WithBase58Shortener()
+	var shortener short.Shortener = short.NewSha256WithBase58Shortener()
 	var storage stor.Storage = stor.NewInMemoryStorage()
 	createHandler := h.NewCreateHandler(storage, shortener)
 	redirectHandler := h.NewRedirectHandler(storage)
