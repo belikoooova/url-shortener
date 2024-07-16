8e8cd2747727c9e874b29458a08181fa9d28cb2d
Maria Belikova <mebelikova@edu.hse.ru>
2024-07-16 23:24:36 +0300

    Try to fix port (8)

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -6,7 +6,7 @@ var AppRunServerAddressFromFlag string
 var RedirectAddressFromFlag string
 
 func ParseFlags() {
-	flag.StringVar(&AppRunServerAddressFromFlag, "a", "localhost:8080", "Address to listen for incoming requests")
-	flag.StringVar(&RedirectAddressFromFlag, "b", "localhost:8080", "Address to redirect")
+	flag.StringVar(&AppRunServerAddressFromFlag, "a", ":8080", "Address to listen for incoming requests")
+	flag.StringVar(&RedirectAddressFromFlag, "b", ":8080", "Address to redirect")
 	flag.Parse()
 }
