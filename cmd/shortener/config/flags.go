ea4f04319d9a939c47c56981c8560e0cb95e775d
Maria Belikova <mebelikova@edu.hse.ru>
2024-07-16 23:50:07 +0300

    Fix empty flag (2)

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -9,4 +9,11 @@ func ParseFlags() {
 	flag.StringVar(&AppRunServerAddressFromFlag, "a", ":8080", "Address to listen for incoming requests")
 	flag.StringVar(&RedirectAddressFromFlag, "b", ":8080", "Address to redirect")
 	flag.Parse()
+
+	if AppRunServerAddressFromFlag == "" {
+		AppRunServerAddressFromFlag = ":8080"
+	}
+	if RedirectAddressFromFlag == "" {
+		RedirectAddressFromFlag = ":8080"
+	}
 }
