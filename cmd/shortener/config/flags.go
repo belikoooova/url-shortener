cbd947ffd55b6c4dafa380c175c3de22973a2f88
Maria Belikova <mebelikova@edu.hse.ru>
2024-07-17 10:14:14 +0300

    Fuck this shit

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -6,8 +6,8 @@ var AppRunServerAddressFromFlag string
 var RedirectAddressFromFlag string
 
 func ParseFlags() {
-	flag.StringVar(&AppRunServerAddressFromFlag, "a", ":8080", "Address to listen for incoming requests")
-	flag.StringVar(&RedirectAddressFromFlag, "b", ":8080", "Address to redirect")
+	flag.StringVar(&AppRunServerAddressFromFlag, "a", "localhost:8080", "Address to listen for incoming requests")
+	flag.StringVar(&RedirectAddressFromFlag, "b", "http://localhost:8080", "Address to redirect")
 	flag.Parse()
 
 	if AppRunServerAddressFromFlag == "" {
--- internal/app/model/url.go
+++ internal/app/model/url.go
@@ -1,7 +1,5 @@
 package model
 
-const BaseURL = "http://172.17.0.1"
-
 type URL struct {
 	ID          string `json:"id"`
 	OriginalURL string `json:"original_url"`
